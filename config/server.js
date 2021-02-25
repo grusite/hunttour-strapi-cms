9fedf26f1eefdc5f5b53fd3d375c03918058ee7c
Jorge Martín <grusite@gmail.com>
2021-02-25 19:55:26 +0100

    init strapi with mongo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd2342ac65ba9f90ac8f0b33231f7d875'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bbe138e5-763e-40b6-8071-baab5db43c42'
+};
