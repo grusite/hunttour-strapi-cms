93aea605d023f1c3f721e5dd95779cec784d360f
Jorge Martín <grusite@gmail.com>
2021-03-21 03:19:31 +0100

    managing envinronments dev and prod for database and cloudinary plugin

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Strapi application
 
 A quick description of your strapi application
+
+para dev "yarn develop"
+para prod "yarn start"
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,16 @@
+const devMongoUri = 'mongodb://localhost:27017/hunttourStrapiCms'
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'hunttourStrapiCms')
+        uri: env('DATABASE_URI')
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
