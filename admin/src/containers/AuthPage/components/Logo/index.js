b2927b6db9eb89d3f22fc05433477e3281c0b071
Jorge Martín <grusite@gmail.com>
2021-05-26 00:35:36 +0200

    changed page Logo, login Logo and LeftMenu Logo

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import LogoStrapi from '../../../../assets/images/logo_strapi.png';
+import LogoStrapi from "../../../../assets/images/key-letters-vertical-2_r@4x.png";
+import Img from "./Img";
+
+const Logo = () => <Img src={LogoStrapi} alt="hunttour-logo" />;
+
+export default Logo;
